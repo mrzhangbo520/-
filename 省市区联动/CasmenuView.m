193f05018fb76f1696b1105c01c61c55d87c7b20
zhangbo1993 <1747680570@qq.com>
2018-10-10 16:45:30 +0800

    添加省市json文件

---
+++ 省市区联动/CasmenuView.m
@@ -0,0 +1,21 @@
+//
+//  CasmenuView.m
+//  省市区联动
+//
+//  Created by jx on 2018/10/10.
+//  Copyright © 2018年 jx. All rights reserved.
+//
+
+#import "CasmenuView.h"
+
+@implementation CasmenuView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
