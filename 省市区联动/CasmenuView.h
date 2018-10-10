8a26a382c8747ee92aea1e0059c0f476eb80b043
zhangbo1993 <1747680570@qq.com>
2018-10-10 16:41:23 +0800

    测试

---
+++ 省市区联动/CasmenuView.h
@@ -0,0 +1,13 @@
+//
+//  CasmenuView.h
+//  省市区联动
+//
+//  Created by jx on 2018/10/10.
+//  Copyright © 2018年 jx. All rights reserved.
+//
+#define tableviewWideth1 kScreen_Wideth/2.0
+#define tableviewWideth2 kScreen_Wideth/3.0
+#import <UIKit/UIKit.h>
+
+@interface CasmenuView : UIView
+@end
